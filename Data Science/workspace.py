9b518051f2a035a41f8ceedf99f247932df0f1e8
Chris Colella <chriscolella3@gmail.com>
2022-09-19 12:27:04 -0400

    Class Excersises

---
+++ Data Science/workspace.py
@@ -0,0 +1,31 @@
+#Excersises
+
+#def my_function(i):
+#    print (i)
+
+#while i <= 10:
+#    my_function(i)
+#    i += 1
+
+#i = 1
+#x = 10
+#sum = 0
+
+#def sum_numbers(i, x, sum):
+#        for i in range(x+1):
+#            sum += i
+#            i += 1
+#            print (sum)
+
+#sum_numbers(i, x, sum)
+
+x = [1,2,3]
+y = ['a','b','c',]
+
+print (x[0])
+x.append(4)
+print (x)
+x[1] = 100
+x.pop(0)
+print (x)
+print (y[0])
