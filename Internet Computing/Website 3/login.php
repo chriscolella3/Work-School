c6888d15284f0f5e0edc2c45c80e499a8458457a
Chris Colella <chriscolella3@gmail.com>
2022-10-24 14:38:23 -0400

    Lab 3/Data midterm
    Make sure to put all of this inside htdocs in xampp for the apache server.

---
+++ Internet Computing/Website 3/login.php
@@ -0,0 +1,11 @@
+<?php
+// Written by Chris Colella 10/30/2022
+
+$incomingpassword = $_POST['pswd'];
+$incomingusername = $_POST['userName'];
+
+echo 'Thank you ';
+echo $_POST['userName'];
+echo ' your password is ';
+echo $_POST['pswd'];
+?>
---
+++ Internet Computing/Website 3/register.php
@@ -0,0 +1,30 @@
+<?php
+// Written by Chris Colella 10/30/2022
+
+$incomingnewpassword = $_POST['newPswd'];
+$incomingnewusername = $_POST['newUserName'];
+$duration = $_POST['membership'];
+
+
+if ($duration == '6'){
+    $duration = 25*6;
+} elseif ($duration == '24') {
+    $duration = 25*24;
+}
+
+echo 'Welcome ';
+echo $_POST['newUserName'];
+echo ' to become a new member.';
+echo ' Your password is ';
+echo $_POST['newPswd'];
+echo  nl2br ("\nYour membership length is ");
+echo $_POST['membership'];
+echo ' months.';
+echo  nl2br ("\nThe total charge of your membership is $");
+echo $duration;
+echo '.';
+
+
+
+
+?>
